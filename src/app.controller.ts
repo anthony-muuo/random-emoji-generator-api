8ee097f748faecb6b792b3868f09ce07e5d1574a
muuo <anthonymuuo92@gmail.com>
2025-07-29 09:34:52 +0300

    returning the browser in the response has nothing to do with random emoji generation. Its included to show how you can leverage changes to the incoming HTTP request that were implemented earlier on in the NestJS lifecycle (in the browser interceptor, in this case)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EmojiValidationPipe } from './common/emoji-validation/emoji-validation.pipe';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getEmoji(@Query('index', EmojiValidationPipe) index?: number): string {
+  getEmoji(
+    @Req() request: Request,
+    @Query('index', EmojiValidationPipe) index?: number,
+  ) {
     console.log(`this is the index logged out of the emojii ${index}`);
-    return this.appService.getEmoji(index);
+    return {
+      browser: request.headers.browser,
+      emoji: this.appService.getEmoji(index),
+    };
   }
 }
