43c2b3cb81c17b6e7dc601666d6ff22c5c096ae5
muuo <anthonymuuo92@gmail.com>
2025-07-29 10:49:35 +0300

    built my custom httpexception to catch all errors thrown from the server

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -8,6 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    throw new UnauthorizedException();
     const request = context.switchToHttp().getRequest();
     const apiKey = request.headers['x-api-key'];
     if (apiKey !== 'SECRET') {
